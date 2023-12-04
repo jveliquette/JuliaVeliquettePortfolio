30c0e7358a4c7f5367b6aad35cf06462b8ca46ad
jveliquette <juliaveliquette@yahoo.com>
2023-12-04 11:40:03 -0800

    JavaScript Comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// This function toggles the styling of my hamburger icon. The "open" class is used in CSS to control the appearance of the hamburger menu.
+// Calling this function switches between the open and closed states of the menu and icon.
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
